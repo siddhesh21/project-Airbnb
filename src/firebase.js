15e6ee983767366fd6f55c386bfad7310af595af
Siddhesh Kankekar <kankekas@tcd.ie>
2021-05-07 08:06:33 +0100

    initiate Home + Header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAcEoO_7K-F6wZd_Yed2JvfP8QxV47AlVA",
+  authDomain: "project-airbnb-909bd.firebaseapp.com",
+  projectId: "project-airbnb-909bd",
+  storageBucket: "project-airbnb-909bd.appspot.com",
+  messagingSenderId: "232401963179",
+  appId: "1:232401963179:web:65c1b695748558f6a14fee",
+  measurementId: "G-RLGHL0TD2H",
+};
